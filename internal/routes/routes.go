f87b31ae9c1eb2952f3336d4669754ab93abe985
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-11 10:29:16 +0700

    map routes SETUP

---
+++ internal/routes/routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nt2311-vn/go-fiber/internal/handlers"
+)
+
+func Setup(app *fiber.App) {
+	app.Get("/", handlers.HomeHandler)
+}
