2175cbe7d1cedd7999e785d68cd706f2c43fce09
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-11 11:11:09 +0700

    render login page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,15 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nt2311-vn/go-fiber/components"
+)
 
 func HomeHandler(c *fiber.Ctx) error {
 	return c.SendString("Hello World")
+}
+
+func LoginPage(c *fiber.Ctx) error {
+	loginPage := components.Login()
+	return Render(loginPage)(c)
 }
