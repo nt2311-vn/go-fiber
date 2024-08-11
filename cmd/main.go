6b9efef3df6b2c2c160f3f95561aed30b8eee026
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-11 10:05:50 +0700

    sereve static and simple hello world on main page

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"path/filepath"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+
+	app := fiber.New()
+	slog.Info("Serving web app on", "address", listenAddr)
+	app.Static("/static", filepath.Join("static"))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!")
+	})
+
+	app.Listen(listenAddr)
+}
