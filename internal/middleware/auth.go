53fa336c0f12cd8f42ec5c98f7548b54ef5549eb
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-12 01:06:31 +0700

    auth packages

---
+++ internal/middleware/auth.go
@@ -0,0 +1,32 @@
+package middleware
+
+import "github.com/gofiber/fiber/v2"
+
+var PublicPaths = map[string]bool{
+	"/":                   true,
+	"/login":              true,
+	"/login/jira":         true,
+	"/auth/jira/callback": true,
+}
+
+func AuthMiddleWare(c *fiber.Ctx) error {
+	isAuthenticated := c.Get("X-Dev-Access") == "true"
+
+	if isPublicPath(c.Path()) {
+		return c.Next()
+	}
+
+	if !isAuthenticated {
+		return c.Redirect("/login", fiber.StatusSeeOther)
+	}
+
+	return c.Next()
+}
+
+func IsAuthenticated(c *fiber.Ctx) bool {
+	return c.Get("X-Dev-Access") == "true"
+}
+
+func isPublicPath(path string) bool {
+	return PublicPaths[path]
+}
