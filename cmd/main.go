75baf9ca706d0a05f631676e39af3e20bdd3a571
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-11 10:29:04 +0700

    serving info

--- cmd/main.go
+++ cmd/main.go
@@ -13,16 +13,14 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		panic(err)
 	}
-
 	listenAddr := os.Getenv("LISTEN_ADDR")
 
 	app := fiber.New()
-	slog.Info("Serving web app on", "address", listenAddr)
 	app.Static("/static", filepath.Join("static"))
-
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World!")
 	})
 
 	app.Listen(listenAddr)
+	slog.Info("Serving web app on", "address", listenAddr)
 }
