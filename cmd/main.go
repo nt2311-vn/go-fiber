a298f41001cb78b69074eecc72d4e5068bda2fec
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-12 01:06:50 +0700

    certkey for https

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"github.com/nt2311-vn/go-fiber/internal/middleware"
 	"github.com/nt2311-vn/go-fiber/internal/routes"
 )
 
@@ -15,12 +16,18 @@ func main() {
 		panic(err)
 	}
 	listenAddr := os.Getenv("LISTEN_ADDR")
+	certFile := filepath.Join("localhost.pem")
+	keyFile := filepath.Join("localhost-key.pem")
 
 	app := fiber.New()
 	app.Static("/static", filepath.Join("static"))
+	app.Use(middleware.AuthMiddleWare)
 
 	routes.Setup(app)
 
-	app.Listen(listenAddr)
+	if err := app.ListenTLS(listenAddr, certFile, keyFile); err != nil {
+		slog.Error("Failed to start server", "error", err)
+	}
+
 	slog.Info("Serving web app on", "address", listenAddr)
 }
