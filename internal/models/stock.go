c6774cedecd7ad06d5031f4b09545decfd303f4c
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-16 17:23:56 +0700

    model stock

--- internal/models/stock.go
+++ internal/models/stock.go
@@ -6,12 +6,19 @@ type InvItem struct {
 	InvName      string
 	InvPkgSize   string
 	InvStockUnit string
+	InvUnit1     string
+	InvRate1     float64
+	InvUnit2     string
+	InvRate2     float64
+	InvUnit3     string
+	InvRate3     float64
+	RequestQty   uint32
 }
 
 type StockIssue struct {
 	StockIssueID int
 	Custommer    *CustomerInfo
-	Warehous     WarehouseInfo
+	Warehouse    WarehouseInfo
 }
 
 type CustomerInfo struct {
